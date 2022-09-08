137927f7d2bc78d3fa2295808d18d572d2e933a9
unknown <asmatol@yandex.ru>
2022-09-08 15:14:59 +0300

    перезалив из за роутинга2

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -23,7 +23,7 @@ export const Header:FC = () => {
                         </div>
                     </div>
                 </Link>
-                {pathname === '/pizza-react'  && <Search/> }
+                {pathname === '/pizza-react/'  && <Search/> }
                 {totalCount > 0 && <div className="header__cart">
                     {pathname !== 'pizza-react/basket' &&
                         <Link to='pizza-react/basket' className="button button--cart">
